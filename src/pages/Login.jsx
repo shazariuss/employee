eb8bf3afc384b872a462852b16b9c9ddd18b3e09
shazariuss <shazariuss@gmail.com>
2024-12-04 14:24:40 +0500

    register added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -86,6 +86,12 @@ const Login = () => {
                         >
                             Sign in
                         </button>
+                        <button
+                            onClick={() => navigate("/register")}
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-black   focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Register
+                        </button>
                     </div>
                 </form>
             </div>
